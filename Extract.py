9ab016a6fcd44bfaa1219268a0b1613e999dabde
Lac089 <112132737+Lac089@users.noreply.github.com>
2022-09-07 11:41:59 -0500

    commit all changes

--- Extract.py
+++ Extract.py
@@ -1,6 +1,67 @@
 import sqlite3
 import csv
 
+def list_rows(path):
+    """return a file as a list of rows
+
+    Keyword arguments:
+    path: The path of the file to open
+    """
+    with open(path) as file: 
+        reader = csv.reader(file) 
+        rows = []
+        for row in reader:
+            rows.append(row)
+        del rows[0]
+        return rows
+
+def real_Date(date,time):
+    """Receive a date in format 'mm/dd/yyyy' and a time and return a TIMESTAMP in format 'yyy-mm-dd hh:mm:ss'
+
+    Keyword arguments:
+    date: String date in mm/dd/yyyy format
+    time: String time in hh:mm:ss
+    """
+    
+    pos = date.find('/')
+    year = date[-4:]
+    day = date[pos+1:pos+3]
+    if day[1] == '/':
+        day = '0' + day[0]
+    month = date[:pos]
+    if len(month) == 1:
+        month = '0' + month
+    real = year + '-' + month + '-' + day + ' ' + time
+    return real
+
+def ins_type(type):
+    """Insert each unique type in the Type Table"""
+    cur.execute('''INSERT OR IGNORE INTO Type (name) VALUES ( ? )''', ( type, ) )
+    cur.execute('SELECT id FROM Type WHERE name = ? ', (type, ))
+    return cur.fetchone()[0]
+    
+def ins_recipe(recipe, type_id ):
+    """Insert each unique recipe in the Recipe Table"""
+    cur.execute('''INSERT OR IGNORE INTO Recipe (name, type_id) VALUES ( ? , ? )''', ( recipe, type_id ) )
+    cur.execute('SELECT id FROM Recipe WHERE name = ? ', (recipe, ))
+    return cur.fetchone()[0]
+
+def ins_orders( order_id, date ):
+    """Insert each unique order in the Order Table"""
+    cur.execute('''INSERT OR IGNORE INTO Orders (id, date) VALUES ( ? , ? )''', ( order_id, date ) )
+
+def ins_pizza(order_id, size, recipe_id, price ):
+    """Insert each unique pizza in the Pizza Table"""
+    cur.execute('''INSERT OR IGNORE INTO Pizza (order_id, size, recipe_id, price ) VALUES ( ? , ?, ?, ?)''', ( order_id, size, recipe_id, price ))
+
+def insert_ing(ing,recipe_id):
+    """Insert each unique ingredient in the ingredient Table"""
+    cur.execute('''INSERT OR IGNORE INTO Ingredient (name) VALUES ( ? )''', ( ing, ) )
+    cur.execute('SELECT id FROM Ingredient WHERE name = ? ', (ing, ))
+    ing_id = int(cur.fetchone()[0])
+    cur.execute('''INSERT OR IGNORE INTO Connection (recipe_id, ingr_id) VALUES ( ?,? )''', ( ing_id, recipe_id) )
+
+
 #Create Database pizza.sqlite
 conn = sqlite3.connect('pizza.sqlite')
 cur = conn.cursor()
@@ -45,28 +106,8 @@ CREATE TABLE IF NOT EXISTS Connection (
 
 ''')
 
-#Open file daa.csv
-file = open('data.csv')
-reader = csv.reader(file)
-
-rows = []
-for row in reader:
-        rows.append(row)
-#['49569', '2015-021347', '12/31/2015', '21:14:37', 'peppr_salami', 'S', 'supreme', '12.5']
-
-#Function that recives a date in formar 'mm/dd/yyyy" and a time and returns a TIMESTAMP in format 'yyy-mm-dd hh:mm:ss'
-def real_Date(date,time):
-        pos = date.find('/')
-        year = date[-4:]
-        day = date[pos+1:pos+3]
-        if day[1] == '/':
-                day = '0' + day[0]
-        month = date[:pos]
-        if len(month) == 1:
-                month = '0' + month
-        real = year + '-' + month + '-' + day + ' ' + time
-        return real
-
+#Open file data.csv and create the list of rows
+rows =list_rows('data.csv')
 
 #extract the orderid, date, size, recipe, price, and type of the pizza and insert them in the database
 for row in rows:
@@ -78,43 +119,24 @@ for row in rows:
         price = row[7]
         type = row[6]
 
-        cur.execute('''INSERT OR IGNORE INTO Type (name)
-        VALUES ( ? )''', ( type, ) )
-        cur.execute('SELECT id FROM Type WHERE name = ? ', (type, ))
-        type_id = cur.fetchone()[0]
-        
-        cur.execute('''INSERT OR IGNORE INTO Recipe (name, type_id)
-        VALUES ( ? , ? )''', ( recipe, type_id ) )
-        cur.execute('SELECT id FROM Recipe WHERE name = ? ', (recipe, ))
-        recipe_id = cur.fetchone()[0]
-
-        cur.execute('''INSERT OR IGNORE INTO Orders (id, date)
-        VALUES ( ? , ? )''', ( order_id, date ) )
-
-        cur.execute('''INSERT OR IGNORE INTO Pizza (order_id, size, recipe_id, price )
-        VALUES ( ? , ?, ?, ?)''', ( order_id, size, recipe_id, price ))
-           
+        type_id = ins_type(type)
+        recipe_id = ins_recipe(recipe, type_id )
+        ins_orders( order_id, date )
+        ins_pizza(order_id, size, recipe_id, price )
+                
 conn.commit()
 
-#Open ingredients.txt
-fhand =open('ingredients.txt')
-
+with open('ingredients.txt') as filehandle: 
 #read the file ingredients.txt and insert each ingredient in the database
-
-for line in fhand:
-    if len(line) == 1:
-        continue
-    pos = line.find(':')
-    recipe = line[:pos]
-    cur.execute('SELECT id FROM Recipe WHERE name = ? ', (recipe, ))
-    recipe_id = int(cur.fetchone()[0])
-    ingredients = line[pos+2:].split(", ")
-    for ing in ingredients:
-        cur.execute('''INSERT OR IGNORE INTO Ingredient (name)
-        VALUES ( ? )''', ( ing, ) )
-        cur.execute('SELECT id FROM Ingredient WHERE name = ? ', (ing, ))
-        ing_id = int(cur.fetchone()[0])
-        cur.execute('''INSERT OR IGNORE INTO Connection (recipe_id, ingr_id)
-        VALUES ( ?,? )''', ( ing_id, recipe_id) )
-conn.commit()
+    for line in filehandle:
+        if len(line) == 1:
+            continue
+        pos = line.find(':')
+        recipe = line[:pos]
+        cur.execute('SELECT id FROM Recipe WHERE name = ? ', (recipe, ))
+        recipe_id = int(cur.fetchone()[0])
+        ingredients = line[pos+2:].split(", ")
+        for ing in ingredients:
+            insert_ing(ing,recipe_id)
+    conn.commit()
       
